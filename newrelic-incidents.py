bf4cea7f23fca80f10d9dbb6cee091f181625bad
Bhanu Kiran@gmail.com <bhanu.kiran117@gmail.com>
2020-05-26 14:15:33 +0530

    Added Ack, close functionality

--- newrelic-incidents.py
+++ newrelic-incidents.py
@@ -1,8 +1,8 @@
 import requests
 import json
-import datetime
 
-AUTH_TOK = 'NRRA-xxxxxxxxxxxxxxxxxxxx'
+ 
+AUTH_TOK = 'NRA-xxxxxxxxxxxxxxx'  # For Put requests you have get admin API Token, else you get permission denied.
 HEADERS = {
     'X-Api-Key': '{0}'.format(AUTH_TOK),
     'Content-type': 'application/json',
@@ -10,8 +10,9 @@ HEADERS = {
 
 incident_data_dict = {}
 list_alerts_violations ={}
-#curl -X GET 'https://api.newrelic.com/v2/alerts_incidents/' \
-#     -H 'X-Api-Key:NRRA-0d96bee135fcb3ffc1b878408615e82a7ab8e46841'
+
+
+
 def get_incident(incident_number):
     ivars = dict()
     ivars['base_url'] = "https://api.newrelic.com/v2/alerts_incidents"
@@ -42,6 +43,38 @@ incident_data_dict = get_incident(130772850)
 #print(incident_data_dict)
 
 list_alerts_violations = list_alerts_violations(start_date="2020-05-25T16:55:00+00:00",end_date="2020-05-27T16:56:00+00:00",only_open="true")
-print(list_alerts_violations)
+#print(list_alerts_violations)
 for violations in list_alerts_violations['violations']:
     print(violations['links']['incident_id'])
+    
+
+
+def post_incident_ack(incident_number):
+    ivars = dict()
+    ivars['base_url'] = "https://api.newrelic.com/v2/alerts_incidents"
+    ivars['incident_number'] = incident_number
+    results = requests.put(
+    '{base_url}/{incident_number}/acknowledge.json'.format(**ivars),
+    headers=HEADERS
+    )
+    return "Successful Ack" if results.status_code == 200 else "Failure Ack"
+
+
+def post_incident_close(incident_number):
+    ivars = dict()
+    ivars['base_url'] = "https://api.newrelic.com/v2/alerts_incidents"
+    ivars['incident_number'] = incident_number
+    results = requests.put(
+    '{base_url}/{incident_number}/close.json'.format(**ivars),
+    headers=HEADERS
+    )
+    return "Close Successful" if results.status_code == 200 else "Close Successful"
+
+for violations in list_alerts_violations['violations']:
+    #print(violations['links']['incident_id'])
+    print(violations['links']['incident_id'], post_incident_ack(violations['links']['incident_id']))
+
+
+for violations in list_alerts_violations['violations']:
+    #print(violations['links']['incident_id'])
+    print(violations['links']['incident_id'], post_incident_close(violations['links']['incident_id']))
